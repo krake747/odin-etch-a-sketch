e8661d9b5f326b413c6a8a1e2f6cc1ab024c308a
krake747 <kraemer.kevin747@gmail.com>
2022-06-25 12:47:51 +0200

    Added randomizing grid box color

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
 // Constant variables
 const DEFAULT_SIZE = 16;
+const DEFAULT_COLOR = "green";
 
 // Variables
 let currentSize = DEFAULT_SIZE;
@@ -9,9 +10,11 @@ const grid = document.getElementById("grid");
 const resetBtn = document.getElementById("reset-btn");
 const sizeInput = document.getElementById("size-input");
 const sizeValue = document.getElementById("size-value");
+const rainbowBtn = document.getElementById("rainbow-btn");
 
 // Functions
 let setCurrentSize = (newSize) => currentSize = newSize;
+
 let updateSizeValue = (value) => sizeValue.innerHTML = `${value} x ${value}`;
 
 let setupGrid = (value) => {
@@ -26,7 +29,14 @@ let setupGrid = (value) => {
     };
 
     const boxes = document.querySelectorAll(".box");
-    boxes.forEach(box => box.onmouseover = () => box.style.backgroundColor = "yellow");
+    boxes.forEach(box => box.onmouseover = () => box.style.backgroundColor = DEFAULT_COLOR);
+};
+
+let clearGrid = () => grid.innerHTML = "";
+
+let resetGrid = () => {
+    clearGrid();
+    setupGrid(currentSize);
 };
 
 let updateGrid = (value) => {
@@ -34,16 +44,24 @@ let updateGrid = (value) => {
     updateSizeValue(value);
     resetGrid();
 };
-let clearGrid = () => grid.innerHTML = "";
-let resetGrid = () => {
-    clearGrid();
-    setupGrid(currentSize);
+
+let randomizeGridBoxColor = () => {
+    const boxes = document.querySelectorAll(".box");
+    boxes.forEach(box => box.onmouseover = () => {
+        let r = rgbRange();
+        let g = rgbRange();
+        let b = rgbRange();
+        box.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+    });
 };
+
+let rgbRange = () => Math.floor(Math.random() * 256);
 
 // Event Functions
 resetBtn.onclick = () => resetGrid();
 sizeInput.onmousemove = (e) => updateSizeValue(e.target.value);
 sizeInput.onchange = (e) => updateGrid(e.target.value);
+rainbowBtn.onclick = () => randomizeGridBoxColor();
 
 // Main
 window.onload = () => {
